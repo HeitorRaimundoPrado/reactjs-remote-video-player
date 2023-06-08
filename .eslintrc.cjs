1851470c11bf897c68d7ca683cded260c65973d8
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-06-08 09:40:35 -0300

    feat(home): adding home page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,9 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  'extends': [
+    'plugin:react/recommended'
+  ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../style'
+
+export default function Home() {
+    return (
+        <>
+            <main>
+                <header>
+                        
+                </header>
+                
+            </main>
+        </>
+    )
+}
