a839991ac2f36c6b5c8919f84de64ba5b982acca
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-06-23 20:58:49 -0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,27 @@
 import '../../src/style/Nav.scss'
+import { Outlet, Link } from 'react-router-dom'
 /*import { FaHouse, FaPlay, FaArrowUpFromBracket } from "react-icons/fa6"*/
 
 function Nav() {
   return (
+    /*<nav>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <Link to="/sound">
+      <span className="material-symbols-outlined">
+        library_music
+      </span>
+    </Link>
+    <Link to="/">
+      <span className="material-symbols-outlined">
+        home
+      </span>
+    </Link>
+    <Link to="/youtube">
+      <span className="material-symbols-outlined">
+        play_arrow
+      </span>
+    </Link>
+  </nav>*/
     <nav>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <a href="/sound">
