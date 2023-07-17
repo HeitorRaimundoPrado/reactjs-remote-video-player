9772354f32571072f1d744a32998052aff4ae2a7
Heitor Prado <heitorrdpp@gmail.com>
2023-07-16 22:17:49 -0300

    fix: on Home.jsx div didn't fit content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,6 +15,7 @@ export default function Home(props) {
                     <h2> {props.t('home.header.title')} </h2>
                     <p> {props.t('home.header.firstParagraph')} </p>
                     <p> {props.t('home.header.secondParagraph')} </p>
+                    <p> {props.t('home.header.thirdParagraph')}</p>
                     <a href="https://github.com/HeitorRaimundoPrado/reactjs-remote-video-player"><img src='github.svg' height='20px' width='20x'/>GitHub</a>
                 </header>
                 <AuthLinks removeToken={removeToken}/>
